e51c2948767fc19aa09443b0492dc39f43287451
else6928 <else6928@vandals.uidaho.edu>
2015-04-23 17:40:11 -0700

    14

---
+++ build/android/gen/com/maptest/MaptestAppInfo.java
@@ -0,0 +1,70 @@
+package com.maptest;
+
+import org.appcelerator.titanium.ITiAppInfo;
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiProperties;
+import org.appcelerator.kroll.common.Log;
+
+/* GENERATED CODE
+ * Warning - this class was generated from your application's tiapp.xml
+ * Any changes you make here will be overwritten
+ */
+public final class MaptestAppInfo implements ITiAppInfo
+{
+	private static final String LCAT = "AppInfo";
+
+	public MaptestAppInfo(TiApplication app) {
+	}
+
+	public String getDeployType() {
+		return "development";
+	}
+
+	public String getId() {
+		return "com.maptest";
+	}
+
+	public String getName() {
+		return "maptest";
+	}
+
+	public String getVersion() {
+		return "1.0";
+	}
+
+	public String getPublisher() {
+		return "Shawna Else";
+	}
+
+	public String getUrl() {
+		return "http://";
+	}
+
+	public String getCopyright() {
+		return "2015 by Shawna Else";
+	}
+
+	public String getDescription() {
+		return "";
+	}
+
+	public String getIcon() {
+		return "appicon.png";
+	}
+
+	public boolean isAnalyticsEnabled() {
+		return true;
+	}
+
+	public String getGUID() {
+		return "969e607b-9d68-46ea-a3f3-8211234543e1";
+	}
+
+	public boolean isFullscreen() {
+		return false;
+	}
+
+	public String getBuildType() {
+		return "";
+	}
+}
